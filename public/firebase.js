4a7104f996abc795fb0542f48108c70acec7cedd
syedanarmeen123 <syeyadanarmeenfatima@gmail.com>
2024-11-21 19:51:02 -0800

    firebase hosting

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdTrd7ld5sHI46E_dEkNi7AH93Gn9g8yM",
+  authDomain: "shoe-card.firebaseapp.com",
+  projectId: "shoe-card",
+  storageBucket: "shoe-card.firebasestorage.app",
+  messagingSenderId: "456150902690",
+  appId: "1:456150902690:web:0008a61cc982f92432f4fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
